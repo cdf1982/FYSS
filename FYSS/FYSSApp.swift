0e777cd3a19f24c23d480c2ab46083476162a3cc
Cesare Forelli <cdf1982@icloud.com>
2026-03-08 18:49:10 +0100

    Initial Commit

---
+++ FYSS/FYSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FYSSApp.swift
+//  FYSS
+//
+//  Created by Cesare Forelli on 08/03/26.
+//
+
+import SwiftUI
+
+@main
+struct FYSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
